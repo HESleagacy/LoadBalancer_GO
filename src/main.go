865000569e5810af1bcb5668e84aaa656fa0d965
Sarva Dubey <sarvadubey@gmail.com>
2026-02-07 03:37:46 +0530

    Defined User Function

--- src/main.go
+++ src/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"               // Package used @lines
-	"net/http"          // Package used @lines 14
+	"fmt"               // Package used @lines 33
+	"net/http"          // Package used @lines 14, 54
 	"net/http/httputil" // Package used @lines 18
 	"net/url"           // Package used @lines 22
-	"os"                // Package used @lines
+	"os"                // Package used @lines 34
 )
 
 type Server interface {
@@ -34,3 +34,21 @@ func handleErr(err error) {
 		os.Exit(1)
 	}
 }
+
+type LoadBalancer struct {
+	port            string
+	roundRobinCount int      // Counter
+	servers         []Server // List of Servers
+}
+
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+	return &LoadBalancer{ // Struct defined @line 38
+		port:            port,
+		roundRobinCount: 0, // Initially 0
+		servers:         servers,
+	}
+}
+
+func (lb *LoadBalancer) getNextAvailableServer() Server {}
+
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {} // Proxy Handler Method
