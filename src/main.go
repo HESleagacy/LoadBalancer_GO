85a9e6b46ed67502943d0f580ab0d589f923cc85
Sarva Dubey <sarvadubey@gmail.com>
2026-02-07 03:02:38 +0530

    Added UserDefined datatypes

--- src/main.go
+++ src/main.go
@@ -1,11 +1,36 @@
 package main
 
-import(
-	"net/http/httputil"                      // Package used @lines 
-	"net/url"                                // Package used @lines
+import (
+	"fmt"               // Package used @lines
+	"net/http"          // Package used @lines 14
+	"net/http/httputil" // Package used @lines 18
+	"net/url"           // Package used @lines 22
+	"os"                // Package used @lines
 )
 
-type simpleServer struct{                    // Structure of the Server
-	addr string                              // Address of the Server
-	proxy "httputil.ReverseProxy"            // ReverseProxy to hide sever port from client
+type Server interface {
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, r *http.Request) // forwarded request
+}
+type simpleServer struct {
+	addr  string
+	proxy *httputil.ReverseProxy // Pointer to reverse proxy instance
+}
+
+func newSimpleServer(addr string) *simpleServer { // Create simpleServer
+	serverUrl, err := url.Parse(addr)
+	handleErr(err) // func @ line 31
+
+	return &simpleServer{ // Struct defined @line 16
+		addr:  addr,
+		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
 }
